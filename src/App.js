6990ae52ffd14f63e9f0fb256e5d37e237af36b9
ACER <Rana-MH-1>
2022-11-28 12:10:47 +0100

    ws api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from "react-bootstrap";
 
 function App() {
+  //create a local state
+  const [users, setUsers] = useState([]);
+  /*  ------ fetch() method  .then()/.catch()------------- */
+  /* const getDataUsers= async ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(res=> setUsers(res) )
+    .catch(err=>console.log(err))
+  } */
+  /* const getDataUsers= async()=>{
+    try {
+      const userss = await fetch('https://jsonplaceholder.typicode.com/users')
+      const parsedData = await userss.json()
+      setUsers(parsedData)
+    } catch (error) {
+      console.log(error);
+    }
+  } */
+
+  /* axios for sending HTTP requests --------  */
+  const getDataUsers = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(data);
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    getDataUsers();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user, i) => (
+        <Card key={i} style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>{user.name}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              {user.email}
+            </Card.Subtitle>
+            <Card.Text>
+              {user.phone}
+            </Card.Text>
+            <Card.Link href="#">Details</Card.Link>
+            
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 }
